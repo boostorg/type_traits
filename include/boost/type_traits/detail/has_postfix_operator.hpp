da8a57f9dc44948227a1c50e5678e34edc08e822
jzmaddock <jzmaddock@gmail.com>
2014-03-13 18:20:53 +0000

    Change check to s_check. Fixes https://svn.boost.org/trac/boost/ticket/8542

--- include/boost/type_traits/detail/has_postfix_operator.hpp
+++ include/boost/type_traits/detail/has_postfix_operator.hpp
@@ -138,10 +138,10 @@ no_operator operator,(no_operator, has_operator);
 
 template < typename Lhs >
 struct operator_exists {
-   static ::boost::type_traits::yes_type check(has_operator); // this version is preferred when operator exists
-   static ::boost::type_traits::no_type check(no_operator); // this version is used otherwise
+   static ::boost::type_traits::yes_type s_check(has_operator); // this version is preferred when operator exists
+   static ::boost::type_traits::no_type s_check(no_operator); // this version is used otherwise
 
-   BOOST_STATIC_CONSTANT(bool, value = (sizeof(check(((make<Lhs>() BOOST_TT_TRAIT_OP),make<has_operator>())))==sizeof(::boost::type_traits::yes_type)));
+   BOOST_STATIC_CONSTANT(bool, value = (sizeof(s_check(((make<Lhs>() BOOST_TT_TRAIT_OP),make<has_operator>())))==sizeof(::boost::type_traits::yes_type)));
 };
 
 
