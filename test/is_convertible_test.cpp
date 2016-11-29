c3b38bc3dd0b8720a1bebd81132b0329e98c1429
zerotypos-found <zero.typos.found@gmail.com>
2016-11-29 12:05:30 +0900

    [inspection report] remove tabs and non-ASCII chars, add newline at EOF, fix copyright

--- test/is_convertible_test.cpp
+++ test/is_convertible_test.cpp
@@ -29,8 +29,8 @@ struct derived2 : public middle2 { };
 template<typename T>
 struct test_bug_4530
 {
-	template<typename A>
-	test_bug_4530(A&&, typename boost::enable_if< ::tt::is_convertible<A&&, T> >::type* =0);
+    template<typename A>
+    test_bug_4530(A&&, typename boost::enable_if< ::tt::is_convertible<A&&, T> >::type* =0);
 };
 
 struct A4530
