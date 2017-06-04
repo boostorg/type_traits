a4279375733693089ac799be077b5f106688ae26
jzmaddock <john@johnmaddock.co.uk>
2017-06-04 11:26:20 +0100

    CI: disable tricky_rvalue_test when we know it will fail with msvc-10 and 11.

--- test/tricky_rvalue_test.cpp
+++ test/tricky_rvalue_test.cpp
@@ -6,6 +6,7 @@
 
 #include "test.hpp"
 #include "check_integral_constant.hpp"
+#include <boost/detail/workaround.hpp>
 #ifdef TEST_STD
 #  include <type_traits>
 #else
@@ -18,12 +19,16 @@
 
 TT_TEST_BEGIN(rvalue_reference_test)
 
+#if !(defined(CI_SUPPRESS_KNOWN_ISSUES) && BOOST_WORKAROUND(BOOST_MSVC, <= 1700))
+
 #ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
 BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_reference<int (&&)(int)>::value, true);
 BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_rvalue_reference<int (&)(int)>::value, false);
 BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_rvalue_reference<int (&&)(int)>::value, true);
 BOOST_CHECK_INTEGRAL_CONSTANT((::tt::is_convertible<int&&, int&>::value), false);
 BOOST_CHECK_INTEGRAL_CONSTANT((::tt::is_convertible<int(), int(&&)()>::value), true);
+#endif
+
 #endif
 
 TT_TEST_END
