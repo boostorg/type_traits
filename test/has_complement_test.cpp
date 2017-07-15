7d7322655e909366b94b18d7b5f528456927bac4
Peter Dimov <pdimov@pdimov.com>
2017-07-16 02:06:18 +0300

    Disable g++-7 warnings in a proper manner

--- test/has_complement_test.cpp
+++ test/has_complement_test.cpp
@@ -3,6 +3,10 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#if defined(__GNUC__) && (__GNUC__ >= 7)
+#pragma GCC diagnostic ignored "-Wbool-operation"
+#endif
+
 #ifdef TEST_STD
 #  include <type_traits>
 #else
