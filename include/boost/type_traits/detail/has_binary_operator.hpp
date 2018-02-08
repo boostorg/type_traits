94e54c030dd91e64af28c5a2cfe16f1ef2de9ee3
John Maddock <john@johnmaddock.co.uk>
2018-02-08 19:21:13 +0000

    binary operators: disable new code for older gcc versions

--- include/boost/type_traits/detail/has_binary_operator.hpp
+++ include/boost/type_traits/detail/has_binary_operator.hpp
@@ -31,7 +31,7 @@
 #   endif
 #endif
 
-#if !defined(BOOST_NO_SFINAE_EXPR) && !defined(BOOST_NO_CXX11_DECLTYPE) && !BOOST_WORKAROUND(BOOST_MSVC, < 1900)
+#if !defined(BOOST_NO_SFINAE_EXPR) && !defined(BOOST_NO_CXX11_DECLTYPE) && !BOOST_WORKAROUND(BOOST_MSVC, < 1900) && !BOOST_WORKAROUND(BOOST_GCC, < 40900)
 
 #include <boost/type_traits/integral_constant.hpp>
 #include <boost/type_traits/make_void.hpp>
--- test/has_equal_to_test.cpp
+++ test/has_equal_to_test.cpp
@@ -219,7 +219,7 @@ void specific() {
    BOOST_CHECK_INTEGRAL_CONSTANT((::boost::BOOST_TT_TRAIT_NAME< int* const &, int* &, int & >::value), 0);
    BOOST_CHECK_INTEGRAL_CONSTANT((::boost::BOOST_TT_TRAIT_NAME< int* const &, int* const &, int const & >::value), 1);
 
-#if !defined(BOOST_NO_SFINAE_EXPR) && !defined(BOOST_NO_CXX11_DECLTYPE) && !BOOST_WORKAROUND(BOOST_MSVC, < 1900)
+#if !defined(BOOST_NO_SFINAE_EXPR) && !defined(BOOST_NO_CXX11_DECLTYPE) && !BOOST_WORKAROUND(BOOST_MSVC, < 1900) && !BOOST_WORKAROUND(BOOST_GCC, < 40900)
    // There are some things that pass that wouldn't otherwise do so:
    auto f = []() {};
 #ifndef BOOST_MSVC
