2bb1021fe87c83f7fb13bb54be94669262964921
Daniel James <daniel@calamity.org.uk>
2018-03-14 13:49:25 +0000

    Include yes_no_type.hpp from is_complete.hpp

--- include/boost/type_traits/is_complete.hpp
+++ include/boost/type_traits/is_complete.hpp
@@ -12,6 +12,7 @@
 #include <boost/type_traits/integral_constant.hpp>
 #include <boost/type_traits/remove_reference.hpp>
 #include <boost/type_traits/is_function.hpp>
+#include <boost/type_traits/detail/yes_no_type.hpp>
 #include <boost/config/workaround.hpp>
 
 /*
