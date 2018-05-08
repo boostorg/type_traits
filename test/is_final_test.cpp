01f9a0b43a5015ab77df961d24e65802ca186266
jzmaddock <john@johnmaddock.co.uk>
2018-05-08 18:53:17 +0100

    Add test for const-qualified final classes.

--- test/is_final_test.cpp
+++ test/is_final_test.cpp
@@ -48,10 +48,12 @@ TT_TEST_BEGIN(is_final)
    BOOST_CHECK_SOFT_INTEGRAL_CONSTANT(::tt::is_final<final_UDT>::value, true, false);
    BOOST_CHECK_SOFT_INTEGRAL_CONSTANT(::tt::is_final<final_UDT const>::value, true, false);
    BOOST_CHECK_SOFT_INTEGRAL_CONSTANT(::tt::is_final<final_template<int> >::value, true, false);
+   BOOST_CHECK_SOFT_INTEGRAL_CONSTANT(::tt::is_final<final_template<int> const>::value, true, false);
 #  else
    BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_final<final_UDT>::value, true);
    BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_final<final_UDT const>::value, true);
    BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_final<final_template<int> >::value, true);
+   BOOST_CHECK_INTEGRAL_CONSTANT(::tt::is_final<final_template<int> const>::value, true);
 #  endif
 #else
    std::cout <<
