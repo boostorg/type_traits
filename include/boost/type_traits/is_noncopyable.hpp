0d7d9c72efc3675a81c8ca60b521c570f6020a6a
Peter Dimov <pdimov@pdimov.com>
2018-08-25 15:36:29 +0300

    Add comment describing the use of noncopyable_::base_token

--- include/boost/type_traits/is_noncopyable.hpp
+++ include/boost/type_traits/is_noncopyable.hpp
@@ -19,6 +19,10 @@ namespace boost
 #ifndef BOOST_NONCOPYABLE_BASE_TOKEN_DEFINED
 #define BOOST_NONCOPYABLE_BASE_TOKEN_DEFINED
 
+// boost::noncopyable derives from noncopyable_::base_token to enable us
+// to recognize it. The definition is macro-guarded so that we can replicate
+// it here without including boost/core/noncopyable.hpp, which is in Core.
+
 namespace noncopyable_
 {
     struct base_token {};
