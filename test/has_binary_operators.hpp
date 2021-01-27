95c8a8a56701254ed46e9f1093e0107e1aa29f41
jzmaddock <john@johnmaddock.co.uk>
2021-01-27 19:33:13 +0000

    Mark testing binary member operators as const. Otherwise they generate ambiguities in C++20.

--- test/has_binary_operators.hpp
+++ test/has_binary_operators.hpp
@@ -51,21 +51,21 @@ struct internal_comma2 { ret_with_comma2 operator BOOST_TT_TRAIT_OP (const inter
 struct external_comma2 { };
 ret_with_comma2 operator BOOST_TT_TRAIT_OP (const external_comma2&, const external_comma2&){ return ret_with_comma2(); }
 
-struct returns_int { int operator BOOST_TT_TRAIT_OP (const returns_int&); };
+struct returns_int { int operator BOOST_TT_TRAIT_OP (const returns_int&)const; };
 
-struct returns_void { void operator BOOST_TT_TRAIT_OP (const returns_void&); };
+struct returns_void { void operator BOOST_TT_TRAIT_OP (const returns_void&)const; };
 
-struct returns_void_star { void *operator BOOST_TT_TRAIT_OP (const returns_void_star&); };
+struct returns_void_star { void *operator BOOST_TT_TRAIT_OP (const returns_void_star&)const; };
 
-struct returns_double { double operator BOOST_TT_TRAIT_OP (const returns_double&); };
+struct returns_double { double operator BOOST_TT_TRAIT_OP (const returns_double&)const; };
 
 struct ret1 { };
 struct convertible_to_ret1 { operator ret1 () const; };
-struct returns_convertible_to_ret1 { convertible_to_ret1 operator BOOST_TT_TRAIT_OP (const returns_convertible_to_ret1&); };
+struct returns_convertible_to_ret1 { convertible_to_ret1 operator BOOST_TT_TRAIT_OP (const returns_convertible_to_ret1&)const; };
 
 struct convertible_to_ret2 { };
 struct ret2 { ret2(const convertible_to_ret2); };
-struct returns_convertible_to_ret2 { convertible_to_ret2 operator BOOST_TT_TRAIT_OP (const returns_convertible_to_ret2&); };
+struct returns_convertible_to_ret2 { convertible_to_ret2 operator BOOST_TT_TRAIT_OP (const returns_convertible_to_ret2&)const; };
 
 class Base1 { };
 class Derived1 : public Base1 { };
